7696893147140ed5ea5f1605cbce5862e29f3739
Qiqiqiqiqishuo <980980068@qq.com>
2021-02-06 22:42:27 +0800

    love for all, hatred for none

--- Week_03/33. Search in Rotated Sorted Array.c
+++ Week_03/33. Search in Rotated Sorted Array.c
@@ -1,35 +1,34 @@
-//-----------------------------------------------------------------------
+//---------------------------------------------------------------------------
 // 1. 不讲武德之暴力遍历 O(n) O(1)
-//万万不可使用，这个方法直接免提
+//  阅后即焚，此法免提
 int search(int* nums, int numsSize, int target){
-    if (!nums || 0 == numsSize) return -1;
-    for (int i=0; i<numsSize; i++) {
+    for (int i=0; i<numsSize; i++)
         if (target == nums[i])  return i;
-    }
     return -1;
 }
 
 
 
-//-------------------------------------------------------------------------
-// 2. 分割出有序数组二分查找 O(logn) O(1)
+//--------------------------------------------------------------------------
+// 2. 减治二分 O(logn) O(1)
+//  **尚未完全理解**
 int search(int* nums, int numsSize, int target){
-    int left = 0, right = numsSize - 1, mid;
+    int left = 0;
+    int right = numsSize - 1;
+    int mid = 0;
     while (left <= right) {
-        mid = left + (right - left) / 2;  // 防止溢出
-        if (nums[mid] == target) return mid;
-        if (nums[left] <= nums[mid]) {  // 等于处理单个不存在情况
-            if (target < nums[mid] && target >= nums[left]) {
-                right = mid - 1;
+        mid = left + (right - left) / 2;
+        if (target == nums[mid])    return mid;
+        if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid -1;
             } else {
-                left = mid + 1;
+                left = mid +1;
             }
+        } else if (target > nums[mid] && target <= nums[right]) {
+            left = mid + 1;
         } else {
-            if (target > nums[mid] && nums[right] >= target) {
-                left = mid + 1;
-            } else {
                 right = mid - 1;
-            }
         }
     }
     return -1;
